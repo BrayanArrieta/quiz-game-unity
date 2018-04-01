9a752dcd263a71774231b33bd9d6c09284244439
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-31 18:10:12 -0600

    Integrate firebase database

---
+++ quiz-game-unity/Assets/FirebaseReadTest.cs
@@ -0,0 +1,14 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+public class FirebaseReadTest: MonoBehaviour {
+	void Start() {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://quiz-game-unity.firebaseio.com/");;
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.LogLevel = LogLevel.Verbose;
+		FirebaseDatabase.DefaultInstance.LogLevel = LogLevel.Verbose;
+		// Get the root reference location of the database.
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("Users");
+	}
+}
